39d6b9b3a5eaaaa9d531c7a89364b77b4ab8c114
NiloyBlueee <niloynilblue@gmail.com>
2025-07-12 23:51:59 +0600

    iteration example

--- LAB2/H.py
+++ LAB2/H.py
@@ -22,3 +22,14 @@ query = int(input())
 for _ in range(query):
     k, x = map(int, input().split())
     print(find_not_divisible(k, x))
+
+"""
+| Iteration | left                             | right | mid | not\_divisible = mid - mid//x | k reached? | action                                         | answer |
+| --------- | -------------------------------- | ----- | --- | ----------------------------- | ---------- | ---------------------------------------------- | ------ |
+| 1         | 1                                | 14    | 7   | 7 − 2 = 5                     | 5 < 7      | move **left = mid + 1** ⇒ 8                    | 0      |
+| 2         | 8                                | 14    | 11  | 11 − 3 = 8                    | 8 ≥ 7      | **answer = 11**, move **right = mid − 1** ⇒ 10 | 11     |
+| 3         | 8                                | 10    | 9   | 9 − 3 = 6                     | 6 < 7      | left = 10                                      | 11     |
+| 4         | 10                               | 10    | 10  | 10 − 3 = 7                    | 7 ≥ 7      | **answer = 10**, right = 9                     | 10     |
+| stop      | left = 10, right = 9 → loop ends |       |     |                               |            |                                                | **10** |
+
+"""
